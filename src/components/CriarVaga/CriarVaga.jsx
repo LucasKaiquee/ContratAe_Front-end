278384ebd26ad1d99df7c429333cb2c1cadd553e
steph4nn <laurostephan@gmail.com>
2023-12-11 20:48:46 -0300

    FEAT: componente CriarVaga finalizado

--- src/components/CriarVaga/CriarVaga.jsx
+++ src/components/CriarVaga/CriarVaga.jsx
@@ -6,23 +6,25 @@ import "./CriarVaga.css"
 export default function CriarVaga(){
     return(
         <section className="criar-vaga">
-            <form className="form-vaga">
+            <div className="title">
                 <h1>criar vaga</h1>
+            </div>
+            <form className="form-vaga">
                 <div className="input-section">
                 	<div className="input-left-section">
-                            <input type="text" name="vaga" placeholder="Título da vaga" />
-                        	<input type="text" name="requisitos" placeholder="Requisitos" />
-                        	<input type="text" name="tipo" placeholder="Tipo" />
-                        	<input type="text" name="desc" className="text-box" placeholder="Descrição" />
+                            <input type="text"className="input-border" name="vaga" placeholder="Título da vaga" />
+                        	<input type="text"className="input-border" name="requisitos" placeholder="Requisitos" />
+                        	<input type="text" className="input-border"name="tipo" placeholder="Tipo" />
+                        	<textarea className="text-box"name="desc-txt" id="desc-txt" cols="30" rows="10" placeholder="Descrição"></textarea>
                     </div>
                     <div className="input-right-section">    
-                    	<input type="text" name="desc" placeholder="Área" />
-                    	<input type="text" name="desc" placeholder="Empresa" />
-                    	<input type="text" name="desc" placeholder="Salário" />
-                    	<input type="text" name="desc" placeholder="Limite da candidaturas" />
+                    	<input type="text" className="input-border"name="desc" placeholder="Área" />
+                    	<input type="text"className="input-border" name="desc" placeholder="Empresa" />
+                    	<input type="text"className="input-border" name="desc" placeholder="Salário" />
+                    	<input type="text"className="input-border" name="desc" placeholder="Limite da candidaturas" />
                     </div>
                 </div>
-                <button className="form-btn">Criar Vaga</button>
+            <button className="form-btn">Criar Vaga</button>
             </form>
         </section>
     )
